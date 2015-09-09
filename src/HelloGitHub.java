21c6e78dc6eb6370d04b7806abd196e38a33c723
wtrammer <wolfgang.trammer@nts.eu>
2015-09-09 14:10:44 +0200

    2nd developer

--- src/HelloGitHub.java
+++ src/HelloGitHub.java
@@ -4,7 +4,7 @@ public class HelloGitHub {
 	public static void main(String[] args) {
 		
 		
-		System.out.println("Hello GitHub!");
+		System.out.println("Hello GitHub, I'm developer 2!");
 		
 		
 
