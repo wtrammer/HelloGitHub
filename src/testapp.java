c2beda5ce22ac2a36a78a7a57b3cf6cac3585b19
wtrammer <wolfgang.trammer@nts.eu>
2015-09-30 21:15:52 +0200

    test

---
+++ src/testapp.java
@@ -0,0 +1,27 @@
+
+public class testapp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			
+		int wert = 50;
+		
+		int i = 2;
+		
+		while (i <=9)
+		{
+			wert = wert * i;
+			System.out.println(wert);
+			i++;
+			
+		}
+		i=2;
+		while (i <=9)
+		{
+			wert = wert / i;
+			System.out.println(wert);
+			i++;
+		}
+	}
+
+}
